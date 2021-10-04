5b81ab745eeb821fd6534d29db6f667ef984ac74
tjatoba <tmjatob@gmail.com>
2021-10-04 11:07:30 -0300

    add while and template string

--- guess-number-game.py
+++ guess-number-game.py
@@ -4,16 +4,29 @@ print("********************************")
 
 secret_number = 42
 
-user_guess_str = input("Digite seu número: ")
+rounds = 3
+round_number = 1
 
-print("Você digitou ", user_guess_str)
+while round_number <= rounds:
+    # print("Rodada", round_number, "de", rounds) -> maneira mais rudimentar
+    print("Rodada {} de {}".format(round_number, rounds))
+    user_guess_str = input("Digite seu número: ")
+    print("Você digitou ", user_guess_str)
+    user_guess = int(user_guess_str)
 
-user_guess = int(user_guess_str)
+    right_choice = secret_number == user_guess
+    higher = user_guess > secret_number
+    lower = user_guess < secret_number
 
-if secret_number == user_guess:
-    print("Você Acertou")
+    if right_choice:
+        print("Você Acertou")
 
-else:
-    print("Que pena! Você errou... :(")
+    else:
+        if higher:
+            print("Foi quase... numero chutado é maior que o número secreto!")
+        elif lower:
+            print("Por pouco... seu número chutado foi menor que o número secreto!")
+
+    round_number = round_number + 1
 
 print("FIM DO JOGO!")
