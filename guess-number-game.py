24d34e6949a1b35901a1a96af16efa9fb4d2e553
Thiago Jatobá <tmjatob@gmail.com>
2021-10-18 11:16:25 -0300

    Conditional names update (#2)
    * cerating my first python program
    
    * add while and template string
    
    * Add conditional names and refact project

--- guess-number-game.py
+++ guess-number-game.py
@@ -4,16 +4,36 @@ print("********************************")
 
 secret_number = 42
 
-user_guess_str = input("Digite seu número: ")
+rounds = 3
+round_number = 1
 
-print("Você digitou ", user_guess_str)
+# while round_number <= rounds:
+    # print("Rodada", round_number, "de", rounds) -> maneira mais rudimentar
+for round_number in range(1, rounds + 1):
+    print("Rodada {} de {}".format(round_number, rounds))
+    # user_guess_str = input("Digite seu número: ")
+    user_guess_str = input("Digite um número entre 1 e 100: ")
+    print("Você digitou ", user_guess_str)
+    user_guess = int(user_guess_str)
 
-user_guess = int(user_guess_str)
+    if user_guess < 1 or user_guess > 100:
+        print("Você deve digitar um número entre 1 e 100")
+        continue
 
-if secret_number == user_guess:
-    print("Você Acertou")
+    right_choice = secret_number == user_guess
+    higher = user_guess > secret_number
+    lower = user_guess < secret_number
 
-else:
-    print("Que pena! Você errou... :(")
+    if right_choice:
+        print("Você Acertou")
+        break
+
+    else:
+        if higher:
+            print("Foi quase... numero chutado é maior que o número secreto!")
+        elif lower:
+            print("Por pouco... seu número chutado foi menor que o número secreto!")
+
+    # round_number = round_number + 1 -> quando escrevo while
 
 print("FIM DO JOGO!")
